b4b28d89a66e2199e78546c5aebf253174fcb407
Ali Moussawi <amm97@mail.aub.edu>
2022-09-30 12:12:47 +0300

    display function

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int verifySelection(int,int);
 void checkForFour();
 
 //global variables
-char board[6][7];
+char board[7][6];
 char name[2][30]; //double dimensional array to store names of the player
 int player; //to store the chance, to track which player is to enter the move
 char gameOptions[2][40]={"Multi-player","alone"};// here we want to specify the option that play can game offer
@@ -18,39 +18,47 @@ int currentPlayer = 0;
 
 //begin main function
 int main() {
-    getInfo();
+    int i;
+    printf("\t\t\t\t\t\tConnect Four Game\n");
+    printf("\t\t\t\t\t\t");
 
-    printf("\n%s, you take Y",name[0]);
-    printf("\n%s, you take X\n",name[1]);
+    for(i=0;i<17;i++)printf("%c",223);
+
     displayBoard();
 
+    getInfo();
+
+    printf("\n\t%s, you have been given Y",name[0]);
+    printf("\t\t\t\t\t%s, you have been given X\n",name[1]);
+
     return;}
+void startGame(){
 
-void displayBoard() {
+return 0;}
+void displayBoard() {// display the game for players
 
-    printf("\t\t\t\t\t-------------\n");
+    printf("\n\n\n\t\t\t\t\t\t  -------------\n");
        int i,j;
-    for(i=0;i<7;i++){
-            printf("\t\t\t\t\t");
-      for(j=0;j<6;j++)
+    for(i=0;i<6;i++){
+            printf("\t\t\t\t\t\t ");
+      for(j=0;j<7;j++)
       {
 	      //printf("element - [%d],[%d] : ",i,j);
 
 	      printf("|%d",board[i][j]);
 	      }
 	      printf("|\n");     }
-    printf("\t\t\t\t\t-------------\n");
+    printf("\t\t\t\t\t\t  -------------\n");
 return 0;
 
 }
-void startGame(){
-
-return 0;}
 int verifySelection(int square, int player) {
     return 0;
 }
 void getInfo(){//get players info
-    printf("Only Multi-player game is allowed in  this version\n");
+    printf("Welcome To Connect Four Game\n");
+    printf("To win you have to be the first player to connect 4  of the same type discs in \na row (either vertically, horizontally, or diagonally)");
+    printf("\nOnly Multi-player option is allowed in  this version\n");
     printf("Enter Player 1 name: ");
     scanf("%s", &name[0]);
     printf("Enter Player 2 name: ");
